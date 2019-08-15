deb71ad1befc57b245d2170982a72e9c76686c1d
zyj <893722059@qq.com>
2019-08-15 13:47:53 +0800

    单例

---
+++ 单例模式/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace 单例模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TaskFactory taskFactory = new TaskFactory();
+            List<Task> taskList = new List<Task>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                taskList.Add(taskFactory.StartNew(() =>
+                {
+                    Singleton singleton = Singleton.getInstance();
+                }));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 单例模式/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 单例模式
+{
+    /// <summary>	
+    /// 描述：
+    /// 创建人： zhaoyongjie
+    /// 创建时间：2019/8/15 11:47:11
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton singleton = null;
+
+        private static object Singleton_Lock = new object(); //锁同步
+        private Singleton()
+        {
+            
+        }
+        public static Singleton getInstance()
+        {
+            lock (Singleton_Lock)
+            {
+                Console.WriteLine("未被创建");
+                if (singleton == null)
+                {
+                    Console.WriteLine("被创建");
+                    singleton = new Singleton();
+                }
+            }
+            return singleton;
+        }
+    }
+}
