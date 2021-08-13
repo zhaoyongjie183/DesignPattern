e6a1d10d678696c523a7ba8be549a5d0708102e0
赵永杰 <893722059@qq.com>
2021-08-13 16:49:16 +0800

    feat：责任链

---
+++ 责任链模式/Bill.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+*┌──────────────────────────────────────────────────────────────┐
+*│　描    述：                                                    
+*│　作    者：赵永杰                                             
+*│　版    本：1.0                                                 
+*│　创建时间：2021-08-13 16:32:57                            
+*└──────────────────────────────────────────────────────────────┘
+*┌──────────────────────────────────────────────────────────────┐
+*│　命名空间： 责任链模式                              
+*│　类    名： Bill                                      
+*└──────────────────────────────────────────────────────────────┘
+*/
+namespace 责任链模式
+{
+    /// <summary>
+    /// 单据状态枚举
+    /// </summary>
+    public enum BillStatus
+    {
+        Open,
+        Saved,
+        Submitted,
+        Audited
+    }
+
+    public abstract class Bill
+    {
+        public string BillName { get; set; }
+
+        public string BilNo { get; set; }
+
+        public BillStatus Status { get; set; }
+
+        public string MaterialName { get; set; }
+
+        public int Qty { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Amount
+        {
+            get
+            {
+                return Qty * Price;
+            }
+        }
+
+        /// <summary>
+        /// 做单员
+        /// </summary>
+        public BillHandler BillMaker { get; set; }
+    }
+
+    public class PurchaseBill : Bill
+    {
+        public PurchaseBill()
+        {
+            base.Status = BillStatus.Open;
+            base.BillName = "采购申请单";
+        }
+    }
+}
---
+++ 责任链模式/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using 责任链模式;
+
+//责任链适用于 工作流、审批流
+
+PurchaseBill bill = new PurchaseBill()
+{
+    BilNo = "CGDD001",
+    MaterialName = "惠普电脑",
+    Qty = 50,
+    Price = 5000,
+    BillMaker = new Purchaser("采购员--小责")
+    //BillMaker = new Manager("经理--任经理")
+    //BillMaker = new CEO("CEO--链总")
+};
+
+Console.WriteLine(string.Format("创建采购申请单：{0};申请购买{1}台{2}", bill.BilNo, bill.Qty, bill.MaterialName));
+
+bill.BillMaker.HandleBill(bill);
+
+Console.ReadLine();
